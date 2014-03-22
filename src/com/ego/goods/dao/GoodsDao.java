944cb784192a4a37dbc611d858695664be3b3d17
iminright <piaokcd@163.com>
2014-03-22 20:10:01 +0800

    Revert "ego"
    This reverts commit 697d9996b12be83ff3b80f7bd6a3b3936ee9c9fe.

--- src/com/ego/goods/dao/GoodsDao.java
+++
@@ -1,8 +0,0 @@
-package com.ego.goods.dao;
-
-import com.ego.goods.model.Goods;
-
-public interface GoodsDao {
-	public void register(Goods goods);
-	public Goods findUserByUserName(final String userName);
-}
